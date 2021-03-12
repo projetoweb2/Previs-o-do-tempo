770233853e042bd7bde1e3a7a7c1c428c6f9922d
Rafael Lucas <arquivoweyne@gmail.com>
2021-03-12 19:22:22 -0300

    Instalando o react Navigation para trabalhar com navegação de páginas

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Esse vai ser meu app tstando de novo</Text>
       <StatusBar style="auto" />
     </View>
   );
