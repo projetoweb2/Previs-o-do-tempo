442e90caf4c671cdb10fc83febfdb0b11f038be9
Rafael Lucas <arquivoweyne@gmail.com>
2021-03-13 10:08:31 -0300

    parte de navegação configurada

--- App.js
+++ App.js
@@ -4,10 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes';
+import { StatusBar } from 'expo-status-bar'; 
 
+//StatusBar hidden={true} esconde a status bar do topo do aplicativo
 export default function App(){
   return(
     <NavigationContainer>
+      <StatusBar hidden={true} />
       <Routes />
     </NavigationContainer>
   )
