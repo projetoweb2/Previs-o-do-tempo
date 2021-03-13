258bd8a49816a598a98261c870bea31873f5fe56
Rafael Lucas <arquivoweyne@gmail.com>
2021-03-13 11:25:57 -0300

    Istalando a biblioteca expo-linear-gradient

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { SafeAreaView, Text, StyleSheet } from 'react-native';
 
 import Menu from '../../components/Menu'; // o '../ sai da pasta e o segundo ../ desce mais uma pasta
+import Header from '../../components/Header';
 
 export default function Home(){
     return(
         <SafeAreaView style={styles.container}>
             <Menu />
+            <Header />
         </SafeAreaView>
     )
 }
