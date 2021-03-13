12fa86cafabec73ab3f55af174013dfdbb1749d2
Rafael Lucas <arquivoweyne@gmail.com>
2021-03-13 11:07:52 -0300

    Estilizando o componente Menu

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,24 @@
 
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+import Menu from '../../components/Menu'; // o '../ sai da pasta e o segundo ../ desce mais uma pasta
 
 export default function Home(){
     return(
-        <SafeAreaView>
-            <Text>Bem vindo ao aplicativo de tempo</Text>
+        <SafeAreaView style={styles.container}>
+            <Menu />
         </SafeAreaView>
     )
 }
+
+//folhas de estilo
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#e8f0ff',
+        paddingTop: '5%'
+    }
+});
