0f0ac434b89af80fb99c4cba35c8a63d3b02de26
Rafael Lucas <arquivoweyne@gmail.com>
2021-03-16 23:26:25 -0300

    .

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -19,7 +19,7 @@ export default function Header(){
                 size={150}
             />
 
-            <Text>30°</Text>
+            <Text style={styles.temp}>30°</Text>
 
         </LinearGradient>
     )
@@ -27,6 +27,24 @@ export default function Header(){
 
 const styles = StyleSheet.create({
     header:{
-
+        width: '95%',
+        height: '55%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8
+    },
+    date:{
+        color: '#FFF',
+        fontSize: 17 
+    },
+    city:{
+        color: '#FFF',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    temp:{
+        color: '#FFF',
+        fontSize: 80,
+        fontWeight: 'bold'
     }
 });
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,109 @@
 
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, FlatList } from 'react-native';
 
 import Menu from '../../components/Menu'; // o '../ sai da pasta e o segundo ../ desce mais uma pasta
 import Header from '../../components/Header';
+import Conditions from '../../components/Conditions';
+import Forecast from '../../components/Forecast';
+
+const mylist = [
+    {
+      "date": "16/03",
+      "weekday": "Ter",
+      "max": 27,
+      "min": 18,
+      "description": "Tempestades",
+      "condition": "storm"
+    },
+    {
+      "date": "17/03",
+      "weekday": "Qua",
+      "max": 28,
+      "min": 18,
+      "description": "Tempestades",
+      "condition": "storm"
+    },
+    {
+      "date": "18/03",
+      "weekday": "Qui",
+      "max": 28,
+      "min": 18,
+      "description": "Tempestades",
+      "condition": "storm"
+    },
+    {
+      "date": "19/03",
+      "weekday": "Sex",
+      "max": 28,
+      "min": 19,
+      "description": "Parcialmente nublado",
+      "condition": "cloudly_day"
+    },
+    {
+      "date": "20/03",
+      "weekday": "Sáb",
+      "max": 27,
+      "min": 18,
+      "description": "Tempestades isoladas",
+      "condition": "storm"
+    },
+    {
+      "date": "21/03",
+      "weekday": "Dom",
+      "max": 28,
+      "min": 18,
+      "description": "Parcialmente nublado",
+      "condition": "cloudly_day"
+    },
+    {
+      "date": "22/03",
+      "weekday": "Seg",
+      "max": 27,
+      "min": 19,
+      "description": "Parcialmente nublado",
+      "condition": "cloudly_day"
+    },
+    {
+      "date": "23/03",
+      "weekday": "Ter",
+      "max": 25,
+      "min": 18,
+      "description": "Tempestades",
+      "condition": "storm"
+    },
+    {
+      "date": "24/03",
+      "weekday": "Qua",
+      "max": 25,
+      "min": 20,
+      "description": "Tempo nublado",
+      "condition": "cloud"
+    },
+    {
+      "date": "25/03",
+      "weekday": "Qui",
+      "max": 25,
+      "min": 18,
+      "description": "Parcialmente nublado",
+      "condition": "cloudly_day"
+    }
+  ];
 
 export default function Home(){
     return(
         <SafeAreaView style={styles.container}>
             <Menu />
             <Header />
+            <Conditions />
+            <FlatList 
+                horizontal={true}
+                contentContainerStyle={{ paddingBottom: '5%'}}
+                style={styles.list}
+                data={mylist}
+                keyExtractor={ item => item.date}
+                renderItem={ ({ item }) => <Forecast data={item}/>}
+            />
         </SafeAreaView>
     )
 }
@@ -22,5 +116,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#e8f0ff',
         paddingTop: '5%'
+    },
+    list:{
+        marginTop: 10,
+        marginLeft: 10
     }
 });
