91e54379b093f485d9710c1232a0507d93e68a44
Rafael Lucas <arquivoweyne@gmail.com>
2021-03-16 21:06:26 -0300

    Corrigindo bug do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Header(){
     return(
-        <View>
-            <Text>HEADER</Text>
-        </View>
+        <LinearGradient 
+        style={styles.header}
+        colors={['#1ed6ff', '#97c1ff']}
+        >
+            <Text style={styles.date}>13/03/2021</Text>
+            <Text style={styles.city}>Fortaleza</Text>
+            
+            <Ionicons 
+                name="cloud"
+                color="#FFF"
+                size={150}
+            />
+
+            <Text>30°</Text>
+
+        </LinearGradient>
     )
 }
+
+const styles = StyleSheet.create({
+    header:{
+
+    }
+});
