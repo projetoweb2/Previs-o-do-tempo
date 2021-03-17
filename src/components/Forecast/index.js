30c43033a626943d374f9fae490a5e692038984f
Rafael Lucas <arquivoweyne@gmail.com>
2021-03-17 19:20:37 -0300

    Correção bug da chamada de variável

--- src/components/Forecast/index.js
+++ src/components/Forecast/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
+import { condition } from '../../utils/condition';
+
 export default function Forecast({ data }){
+    let icon = condition(data.condition)
+
     return(
         <View style={styles.container}>
             <Text style={styles.date}>{data.date}</Text>
-            <Ionicons name="rainy-outline" color="#1ec9ff" size={25}/>
+            <Ionicons name={icon.name} color={icon.color} size={25}/>
             <View style={styles.temp}>
                 <Text>{data.min}°</Text>
                 <Text style={{ fontSize: 18, fontWeight: 'bold'}}>{data.max}°</Text>
@@ -24,10 +28,14 @@ const styles = StyleSheet.create({
         paddingBottom: 10,
         paddingLeft: 14,
         paddingRight: 14,
-        justifyContent: 'center',
+        justifyContent: 'space-around',
         alignItems: 'center'
     },
     date:{
        fontSize: 14, 
+    },
+    temp:{
+        alignItems: 'center',
+
     }
 })
